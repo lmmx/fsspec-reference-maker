4a091c8ffe8b5fa362879214f05f6b3530d1ed80
Louis Maddox <louismmx@gmail.com>
2021-10-22 17:46:43 +0100

    Row-terminating linesep detection

--- fsspec_reference_maker/csv.py
+++ fsspec_reference_maker/csv.py
@@ -4,6 +4,7 @@ import logging
 from typing import BinaryIO, Union
 from io import BytesIO, StringIO
 from csv import reader
+import re
 
 import numpy as np
 import pandas as pd
@@ -15,14 +16,22 @@ import fsspec.utils
 
 lggr = logging.getLogger("csv-to-partitions")
 
-def make_df(n_cols: int, rows_str: str, names: list[str] | None = None) -> pd.DataFrame:
+def make_df(
+    n_cols: int,
+    rows_str: str,
+    sep: str,
+    names: list[str] | None = None,
+) -> pd.DataFrame:
     """
     Read a CSV into a DataFrame without NaN value conversion so that any None values are
     only present due to a missing field, permitting a check for parsed CSV column count.
     """
+    if names is None:
+        names = [str(n) for n in range(n_cols)]
     return pd.read_csv(
         StringIO(rows_str),
         names=names,
+        sep=sep,
         keep_default_na=False,
         na_filter=False,
         na_values=[],
@@ -30,12 +39,15 @@ def make_df(n_cols: int, rows_str: str, names: list[str] | None = None) -> pd.Da
         converters=dict.fromkeys(range(n_cols), trivial_return),
     )
 
+def trivial_return(value):
+    "Return a value unchanged (used to override pandas CSV parser dtype conversion)."
+    return value
+
 def validate_df(
-    r: pd.DataFrame,
+    df: pd.DataFrame,
     escapechar: str = r"\\",
     quotechar: str = '"',
     doublequote: bool = True,
-    verbose: bool = False,
 ):
     """
     Validate that the DataFrame does not contain ``None`` values and does not have
@@ -46,62 +58,19 @@ def validate_df(
     Args:
       df      : (:class:`pd.DataFrame`) DataFrame which can be iterated to give rows to
                 be validated in the form of :class:`pd.Series` objects (one per row).
-      verbose : Whether to print the rows once validated (default: ``False``)
     """
     n_matchable = 1 + int(doublequote)
     re_patt_str = (
         rf".*((?<!({escapechar}))({quotechar}))" + r"{1," + f"{n_matchable}" + r"}.*"
     )
     re_patt = re.compile(re_patt_str)
-    validated_rows = []
-    output = []
     for row_idx, row_series in df.iterrows():
         row = row_series.to_dict()
         if None in row.values():
             raise ValueError(f"Absent field (incomplete row) at {row=}")
         if any(isinstance(v, str) and re_patt.match(v) for v in row.values()):
             raise ValueError(f"{quotechar=} found in {row=}")
-        validated_rows.append(row)
-    if verbose:
-        for row in validated_rows:
-            print(row)  # Only print rows if entire DataFrame validated
     return
-
-def validate_df(
-    df,
-    escapechar=r"\\",
-    quotechar='"',
-    doublequote=True,
-    verbose=True,
-) -> None | list[dict[str, str]]:
-    """
-    Validate that the DataFrame does not contain ``None`` values and does not have
-    ``quotechar`` characters within any field values. Arguments other than those below
-    are the same as those passed to :function:`pandas.read_csv` (``escapechar``,
-    ``quotechar``, ``doublequote``).
-    Args:
-      df      : (:class:`pd.DataFrame`) DataFrame which can be iterated to give rows to
-                be validated in the form of :class:`pd.Series` objects (one per row).
-      verbose : Whether to print the rows once validated (default: ``False``)
-    """
-    n_matchable = 1 + int(doublequote)
-    re_patt_str = (
-        rf".*((?<!({escapechar}))({quotechar}))" + r"{1," + f"{n_matchable}" + r"}.*"
-    )
-    re_patt = re.compile(re_patt_str)
-    validated_rows = []
-    output = []
-    for row_idx, row_series in df.iterrows():
-        row = row_series.to_dict()
-        if None in row.values():
-            raise ValueError(f"Absent field (incomplete row) at {row=}")
-        if any(isinstance(v, str) and re_patt.match(v) for v in row.values()):
-            raise ValueError(f"{quotechar=} found in {row=}")
-        validated_rows.append(row)
-    if verbose:
-        for row in validated_rows:
-            print(row)  # Only print rows if entire DataFrame validated
-    return validated_rows
 
 class SingleCsvToPartitions:
     """Translate the content of one CSV file into CSV partition metadata.
@@ -129,6 +98,8 @@ class SingleCsvToPartitions:
         URI of the CSV file.
     n_columns : int
         The number of columns
+    sep : str
+        Delimiter to use when reading the CSV.
     offsets : list[int]
         The list of offsets into the file at which to seek to place CSV partitions.
     lineseparator : str
@@ -147,6 +118,7 @@ class SingleCsvToPartitions:
         csv: BinaryIO,
         url: str,
         n_columns: int,
+        sep: str = ",",
         blocksize: int = 2 ** 25,
         lineseparator: str = "\n",
         escapechar: str = "\\",
@@ -163,6 +135,8 @@ class SingleCsvToPartitions:
         path = paths[0] # simplified case: presume only handling a single file
         self._size = fs.info(path)["size"]
         lggr.debug(f"Total size: {self._size}")
+        self.n_columns = n_columns
+        self.sep = sep
         self.blocksize = blocksize
         offsets = list(range(0, self._size, blocksize))
         lggr.debug(f"{offsets=}")
@@ -175,6 +149,8 @@ class SingleCsvToPartitions:
         self.spec = spec
         self.storage_options = storage_options or {}
         self.sample_tail_rows = sample_tail_rows
+        self.non_row_term_offsets = []
+        # For the purpose of testing
         # use the CSV handle again here? maybe tail sample buffer?
         # maybe dict of tail sample buffers for each offset?
         #self._csv = csv
@@ -244,8 +220,11 @@ class SingleCsvToPartitions:
 
     def _rowterminatorat(self, offset: int) -> int:
         """
-        Find the first row-terminating lineseparator after the given offset, by reading
-        backwards up to that offset.
+        GOAL: Find the first row-terminating lineseparator after the given offset, by
+        reading backwards up to that offset.
+
+        IMPLEMENTED: Find the first row-terminating lineseparator after the given
+        offset, by matching the first newline character.
         """
         revlinebuf = BytesIO()
         rows_to_sample = self.sample_tail_rows
@@ -258,5 +237,80 @@ class SingleCsvToPartitions:
         while offset > 0 and char != self.linesep:
             char = self.input_file.read(1)
             t += 1
+            if t in self.non_row_term_offsets:
+                pass # Won't re-check, already recorded as non-row-terminating
+            elif char == self.linesep:
+                if not self.linesep_is_row_terminating(t):
+                    self.non_row_term_offsets.append(t)
+            else:
+                continue
+            if t in self.non_row_term_offsets:
+                # Skip any already encountered non-row-terminating newline
+                t += 1
+                # Overwrite the newline char so the while loop doesn't exit
+                char = self.input_file.read(1)
         lggr.debug(f"Transformed {offset} to {t}")
         return t
+
+    def linesep_is_row_terminating(self, offset: int):
+        """
+        Determine if the given position is the location of a row-terminating newline, or
+        merely a newline within a multiline field, by attempting to parse as many rows
+        as specified in the :attr:`sample_tail_rows` attribute (default: 10) and either
+        encountering a parser error (to indicate the newline is not row-terminating)
+        or reaching the start of the file (which would indicate the newline is too early
+        in the file to read all the requested sample tail rows, but is otherwise valid).
+        
+        Args:
+          offset : The position of the newline in the file
+        """
+        starting_tell = self.input_file.tell() # To be restored afterwards
+        line_count = 0
+        rows_str = ""
+        # Step back 2 so that when you read forward 1 you don't get the newline
+        t = offset - 2
+        self.input_file.seek(t)
+        char = ""
+        while t >= 0 and line_count < self.sample_tail_rows:
+            char = self.input_file.read(1)
+            if char == self.linesep:
+                # The offsets are stored with an off-by-one error,
+                # indicating the first character after the newline,
+                # rather than the position of the [non-row-terminating] newline
+                if (t + 1) not in self.non_row_term_offsets:
+                    # Do not increment if the 
+                    line_count += 1
+            rows_str += char
+            t -= 1 # Step back a character to read the next
+            if t >= 0:
+                self.input_file.seek(t)
+            else:
+                # If the start of the file is reached, there's an implicit newline
+                line_count += 1
+        # Now make a df using ``rows_str`` string of ``sample_tail_rows`` many rows
+        if not rows_str.endswith(self.linesep):
+            lggr.debug(f"Adding a linesep to {rows_str!r}")
+            rows_str += self.linesep
+        try:
+            rows_df = make_df(n_cols=self.n_columns, rows_str=rows_str, sep=self.sep)
+        except:
+            # Any ParserError indicates it's not a valid row-terminating offset
+            is_row_terminating = False
+        else:
+            try:
+                validate_df(rows_df)
+            except:
+                # Validation error indicates it's not a valid row-terminating offset
+                is_row_terminating = False
+            else:
+                print(f"Decided {rows_str!r} is row-terminating at {offset}")
+                is_row_terminating = True
+        if not is_row_terminating:
+            print(f"Decided {rows_str!r} is non-row-terminating at {offset}")
+        ## Simulate (testing and) identifying a row terminating newline
+        #if t == 26:
+        #    is_row_terminating = False
+        #else:
+        #    is_row_terminating = True
+        self.input_file.seek(starting_tell)
+        return is_row_terminating
